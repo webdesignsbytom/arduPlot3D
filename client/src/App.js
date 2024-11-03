4228bdf8b11b9943b0443dd648573a43fc009363
Thomas Brockington <xtombrock1989@gmail.com>
2024-11-03 17:57:12 +0000

    save

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import RegisterPage from './users/register/RegisterPage';
 import TestPage from './pages/test/TestPage';
 import Error404 from './pages/error/Error404';
 import LibraryPage from './pages/library/LibraryPage';
-import SimulationDesignPage from './pages/simulation/SimulationDesignPage';
+import SimulationPage from './pages/simulation/SimulationPage';
 import ConfigurationPage from './pages/config/ConfigurationPage';
 // Constants
 import {
@@ -19,21 +19,19 @@ import {
   TEST_PAGE_URL,
   ERROR_404_PAGE_URL,
 } from './utils/Constants';
-import SimulationPage from './pages/simulation/SimulationPage';
 
 function App() {
   return (
-      <Routes>
-        <Route path={HOME_PAGE_URL} index element={<HomePage />} />
-        <Route path={LOGIN_PAGE_URL} element={<LoginPage />} />
-        <Route path={SIGN_UP_PAGE_URL} element={<RegisterPage />} />
-        {/* <Route path={SIMULATION_PAGE_URL} element={<SimulationDesignPage />} /> */}
-        <Route path={'/sim'} element={<SimulationPage />} />
-        <Route path={LIBRARY_PAGE_URL} element={<LibraryPage />} />
-        <Route path={CONFIGURATION_PAGE_URL} element={<ConfigurationPage />} />
-        <Route path={TEST_PAGE_URL} element={<TestPage />} />
-        <Route path={ERROR_404_PAGE_URL} element={<Error404 />} />
-      </Routes>
+    <Routes>
+      <Route path={HOME_PAGE_URL} index element={<HomePage />} />
+      <Route path={LOGIN_PAGE_URL} element={<LoginPage />} />
+      <Route path={SIGN_UP_PAGE_URL} element={<RegisterPage />} />
+      <Route path={SIMULATION_PAGE_URL} element={<SimulationPage />} />
+      <Route path={LIBRARY_PAGE_URL} element={<LibraryPage />} />
+      <Route path={CONFIGURATION_PAGE_URL} element={<ConfigurationPage />} />
+      <Route path={TEST_PAGE_URL} element={<TestPage />} />
+      <Route path={ERROR_404_PAGE_URL} element={<Error404 />} />
+    </Routes>
   );
 }
 
