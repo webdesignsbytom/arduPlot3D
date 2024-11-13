21714271685fa380cab1f1f948941ac6a92bb73b
Thomas Brockington <xtombrock1989@gmail.com>
2024-11-13 21:46:44 +0000

    fixed the nav bar

--- client/src/components/nav/Navbar.jsx
+++ client/src/components/nav/Navbar.jsx
@@ -1,22 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+// Icons
 import { IoMdMenu } from 'react-icons/io';
-import { UserContext } from '../../context/UserContext';
+// Images
 import LogoImage from '../../assets/images/logos/logo (3).png';
 // Context
 import { useModalContext } from '../../context/ModalContext';
+import { useUser } from '../../context/UserContext';
+import { HOME_PAGE_URL } from '../../utils/Constants';
+import useNavigateToPage from '../hooks/useNavigateToPage';
 
 function Navbar() {
-  const { user, setUser } = useContext(UserContext);
-  const { toggleNavigation, toggleNavbarOpenClosed } = useModalContext();
-  const navigate = useNavigate();
+  const { user, setUser } = useUser();
+  const { navigateToPage } = useNavigateToPage();
 
+  const [isPhoneNavOpen, setIsPhoneNavOpen] = useState(false);
+
+  const togglePhoneNav = () => {
+    setIsPhoneNavOpen((prev) => !prev);
+  };
   const handleLogout = (event) => {
     event.preventDefault();
-    toggleNavbarOpenClosed();
     setUser({});
     localStorage.removeItem(process.env.REACT_APP_USER_TOKEN);
-    navigate('/', { replace: true });
+    navigateToPage(HOME_PAGE_URL, { replace: true });
   };
 
   const navItems = [
@@ -37,10 +44,10 @@ function Navbar() {
   ];
 
   return (
-    <nav className='h-full relative z-30 grid grid-cols-reg bg-main-colour py-2 border-b-2 border-solid border-black'>
+    <nav className='relative h-full grid grid-cols-reg px-3 bg-main-colour py-3 border-b-2 border-solid border-black'>
       {/* Logo Section */}
-      <section className='grid items-center justify-center pl-4'>
-        <Link className='no__highlights' to='/'>
+      <section className='grid items-center justify-center'>
+        <NavLink to={HOME_PAGE_URL}>
           <div className='flex'>
             <div>
               <img
@@ -55,84 +62,83 @@ function Navbar() {
               </span>
             </div>
           </div>
-        </Link>
+        </NavLink>
       </section>
 
-      {/* Mobile Nav Toggle */}
-      <section
-        onClick={toggleNavbarOpenClosed}
-        className='grid items-center justify-end lg:hidden no__highlights pr-4 cursor-pointer'
-      >
-        <IoMdMenu className='w-10 h-10 text-colour1 hover:text-hover-grey' />
-      </section>
+      <section className='grid justify-end'>
+        {/* Mobile screen */}
+        <button
+          aria-label='Toggle navigation menu'
+          onClick={togglePhoneNav}
+          className='grid md:hidden w-fit h-fit items-center justify-center text-4xl text-white cursor-pointer'
+        >
+          <IoMdMenu className='active:scale-90 duration-300' />
+        </button>
 
-      {/* Main Navigation */}
-      <section className='hidden lg:grid justify-end'>
-        <ul className='grid grid-flow-col items-center w-fit justify-end gap-4 font-semibold pr-4'>
-          {navItems.map(({ path, label }) => (
-            <li key={path}>
-              <NavLink
-                to={path}
-                className={({ isActive }) =>
-                  `hover:text-colour5 active:scale-95 ${
-                    isActive ? 'text-colour1' : ''
-                  }`
-                }
-              >
-                {label}
-              </NavLink>
-            </li>
-          ))}
-          {user?.email && (
-            <li>
-              <button
-                className='hover:text-colour5 active:scale-95'
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
-            </li>
-          )}
-        </ul>
-      </section>
+        {/* Main Navigation */}
+        <section className='hidden lg:grid justify-end'>
+          <ul className='grid grid-flow-col items-center w-fit justify-end gap-4 font-semibold pr-4'>
+            {navItems.map(({ path, label }) => (
+              <NavItem key={label} url={path} title={label} />
+            ))}
+            {user?.email && (
+              <li>
+                <button
+                  className='hover:text-colour5 active:scale-95'
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </section>
 
-      {/* Mobile Navigation */}
-      {toggleNavigation && (
-        <nav className='absolute lg:hidden w-full left-0 top-24 py-2 px-4'>
-          <div className='bg-black nav__bg p-2 rounded'>
-            <ul className='text-center grid bg-black h-fit w-full text-xl'>
-              {navItems.map(({ path, label }) => (
-                <li key={path}>
-                  <NavLink
-                    to={path}
-                    className={({ isActive }) =>
-                      `w-full no__highlights nav__bg hover:bg-blue-500 active:scale-95 grid py-2 outline-2 outline outline-black ${
-                        isActive
-                          ? 'bg-yellow-700 text-gray-800 font-semibold'
-                          : 'bg-yellow-500'
-                      }`
-                    }
-                  >
-                    {label}
-                  </NavLink>
-                </li>
-              ))}
-              {user?.email && (
-                <li>
-                  <button
-                    className='w-full no__highlights nav__bg hover:bg-blue-500 active:scale-95 grid py-2 outline-2 outline outline-black bg-yellow-500 text-gray-800 font-semibold'
-                    onClick={handleLogout}
-                  >
-                    Logout
-                  </button>
-                </li>
-              )}
-            </ul>
-          </div>
-        </nav>
-      )}
+        {/* Phone navbar */}
+        <div
+          className={`phone-nav absolute top-full left-0 w-full bg-colour2 transition-transform duration-300 ${
+            isPhoneNavOpen
+              ? 'translate-y-0 opacity-100'
+              : '-translate-y-full opacity-0'
+          }`}
+        >
+          <ul className='grid gap-8 items-center justify-center text-center text-orange-600 py-10'>
+            {navItems.map(({ path, label }) => (
+              <NavItem key={label} url={path} title={label} />
+            ))}
+            {user?.email && (
+              <li>
+                <button
+                  className='w-full no__highlights nav__bg hover:bg-blue-500 active:scale-95 grid py-2 outline-2 outline outline-black bg-yellow-500 text-gray-800 font-semibold'
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </section>
     </nav>
   );
 }
+
+const NavItem = ({ url, title }) => {
+  return (
+    <li className='active:scale-90'>
+      <NavLink
+        to={url}
+        aria-label={`${title} page navigation tab`}
+        className='text-xl md:text-lg font-semibold font-poppins hover:brightness-90 duration-200 active:scale-75'
+        aria-current={({ isActive }) => (isActive ? 'page' : undefined)}
+        style={({ isActive }) => {
+          return isActive ? { color: '#f8fafc' } : {};
+        }}
+      >
+        {title}
+      </NavLink>
+    </li>
+  );
+};
 
 export default Navbar;
