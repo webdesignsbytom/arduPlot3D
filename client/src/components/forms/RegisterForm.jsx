30454dd2cfed216e0afd71f19934c6201b66731a
Thomas Brockington <xtombrock1989@gmail.com>
2024-11-17 02:13:02 +0000

    save

--- client/src/components/forms/RegisterForm.jsx
+++ client/src/components/forms/RegisterForm.jsx
@@ -1,61 +1,58 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // API
 import client from '../../api/client';
+// Context
+import { useUser } from '../../context/UserContext';
+// Components
 import LoadingSpinner from '../../components/utils/LoadingSpinner';
 // Utils
-import CountrySelect from '../../utils/CountrySelect';
+import CountrySelect from '../utils/CountrySelect';
+// Constants
+import { LOGIN_PAGE_URL, REGISTER_API } from '../../utils/Constants';
+// Styles
+import { ButtonStyle, InputStyle, LinkStyle } from '../../utils/Styles';
+// Hooks
+import useNavigateToPage from '../../hooks/useNavigateToPage';
 
 function RegisterForm() {
+  const { setUser } = useUser();
+
+  const navigateToPage = useNavigateToPage();
+
   const [registerFormData, setRegisterFormData] = useState({
     email: '',
     password: '',
     confirmPassword: '',
     username: '',
     country: '',
-    termsChecked: true,
-  });
-  const [registerError, setRegisterError] = useState(false);
-  const [registrationFormData, setRegistrationFormData] = useState({
-    active: false,
-    success: false,
+    agreedToTerms: true,
   });
 
-  let navigate = useNavigate();
+  const [registerError, setRegisterError] = useState(null); // Store error message or null
+  const [registrationInProgress, setRegistrationInProgress] = useState(false);
 
-  const loginPage = () => {
-    navigate('/login', { replace: true });
-  };
-
-  const handleSubmitRegisterForm = (event) => {
+  const handleSubmitRegisterForm = async (event) => {
     event.preventDefault();
 
-    setRegistrationFormData({
-      ...registrationFormData,
-      active: true,
-    });
+    setRegistrationInProgress(true);
+    setRegisterError(null); 
 
     client
-      .post('/users/register', registerFormData, false)
-      .then((res) => {
-        setRegistrationFormData({
-          ...registrationFormData,
-          active: false,
-          success: true,
-        });
-        setTimeout(() => {
-          loginPage();
-        }, 2000);
-      })
-
-      .catch((err) => {
-        setRegisterError(true);
-        console.error('Unable to register new user', err);
-      });
+    .post(REGISTER_API, registerFormData, false)
+    .then((res) => {
+      setUser(res.data.existingUser);
+      setRegistrationInProgress(false);
+    })
+    .then(() => navigateToPage(LOGIN_PAGE_URL))
+    .catch((err) => {
+      setRegisterError(err.message);
+      setRegistrationInProgress(false);
+    });
   };
 
   const handleChange = (event) => {
-    setRegisterError(false);
+    setRegisterError(null); // Reset error when input changes
     const { name, value } = event.target;
 
     setRegisterFormData({
@@ -64,52 +61,121 @@ function RegisterForm() {
     });
   };
 
-  const handleChecked = (event) => {
+  const handleCheckboxChange = () => {
     setRegisterFormData({
       ...registerFormData,
-      termsChecked: !registerFormData.termsChecked,
+      agreedToTerms: !registerFormData.agreedToTerms,
     });
   };
+
+  const handleRegisterWith = (service) => {
+    switch (service) {
+      case 'facebook':
+        // Call Facebook login API
+        console.log('Register with Facebook');
+        break;
+      case 'instagram':
+        // Call Instagram login API
+        console.log('Register with Instagram');
+        break;
+      case 'google':
+        // Call Google login API
+        console.log('Register with Google');
+        break;
+      case 'github':
+        // Call GitHub login API
+        console.log('Register with GitHub');
+        break;
+      case 'apple':
+        // Call Apple login API
+        console.log('Register with Apple');
+        break;
+      case 'x':
+        // Call X login API
+        console.log('Register with X');
+        break;
+      default:
+        console.log('Unsupported service');
+    }
+  };
+
   return (
-    <form className='text-center' onSubmit={handleSubmitRegisterForm}>
-      <div className='mb-6'>
+    <form
+      className='grid gap-4'
+      onSubmit={handleSubmitRegisterForm}
+      aria-busy={registrationInProgress}
+    >
+      <div className=''>
+        <label htmlFor='username' className='sr-only'>
+          Username
+        </label>
         <input
           type='text'
-          className='form-control block w-full px-3 py-1.5 text-base font-normal text-colour5 bg-secondary-colour bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-colour5 focus:bg-secondary-colour focus:border-colour2 focus:outline-none'
+          className={InputStyle}
           placeholder='Username'
           id='username'
           name='username'
           onChange={handleChange}
+          required
+          aria-required='true'
         />
       </div>
-      <input
-        type='email'
-        id='email'
-        name='email'
-        className='form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-colour5 bg-secondary-colour bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-colour5 focus:bg-secondary-colour focus:border-colour2 focus:outline-none'
-        placeholder='Email address'
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        id='password'
-        name='password'
-        className='form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-colour5 bg-secondary-colour bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-colour5 focus:bg-secondary-colour focus:border-colour2 focus:outline-none'
-        placeholder='Password'
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        id='confirmPassword'
-        name='confirmPassword'
-        className='form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-colour5 bg-secondary-colour bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-colour5 focus:bg-secondary-colour focus:border-colour2 focus:outline-none'
-        placeholder='Password'
-        onChange={handleChange}
-      />
-      <div className='mb-6'>
+      <div>
+        <label htmlFor='email' className='sr-only'>
+          Email address
+        </label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          className={InputStyle}
+          placeholder='Email address'
+          onChange={handleChange}
+          required
+          aria-required='true'
+          aria-describedby={registerError ? 'email-error' : undefined}
+        />
+      </div>
+      <div>
+        <label htmlFor='password' className='sr-only'>
+          Password
+        </label>
+        <input
+          type='password'
+          id='password'
+          name='password'
+          className={InputStyle}
+          placeholder='Password'
+          onChange={handleChange}
+          required
+          aria-required='true'
+          aria-describedby={registerError ? 'password-error' : undefined}
+        />
+      </div>
+      <div>
+        <label htmlFor='confirmPassword' className='sr-only'>
+          Confirm Password
+        </label>
+        <input
+          type='password'
+          id='confirmPassword'
+          name='confirmPassword'
+          className={InputStyle}
+          placeholder='Confirm Password'
+          onChange={handleChange}
+          required
+          aria-required='true'
+          aria-describedby={
+            registerError ? 'confirm-password-error' : undefined
+          }
+        />
+      </div>
+      <div className=''>
+        <label htmlFor='country' className='sr-only'>
+          Country
+        </label>
         <CountrySelect />
       </div>
-
       <div className='form-check flex justify-center mb-6'>
         <input
           className='form-check-input h-4 w-4 border border-gray-300 rounded-sm transition duration-200 mt-1 align-top float-left mr-2 cursor-pointer checked:bg-colour2'
@@ -117,10 +183,10 @@ function RegisterForm() {
           value=''
           id='termsChecked'
           name='termsChecked'
-          checked
           aria-label='Agree to terms and conditions'
-          style={{ accentColor: 'var(--colour2)' }} // Using `accent-color` here
-          onChange={handleChecked}
+          style={{ accentColor: '#ff0000' }} // Using `accent-color` here
+          checked={registerFormData.agreedToTerms}
+          onChange={handleCheckboxChange}
         />
         <label
           className='form-check-label inline-block text-gray-800'
@@ -135,35 +201,44 @@ function RegisterForm() {
           type='submit'
           data-mdb-ripple='true'
           data-mdb-ripple-color='light'
-          className='inline-block px-6 py-2.5 mb-6 w-full bg-colour2 text-secondary-colour font-medium text-xs leading-tight uppercase rounded shadow-md hover:brightness-110 hover:shadow-lg focus:brightness-110 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'
+          className={ButtonStyle}
+          disabled={registrationInProgress}
+          aria-disabled={registrationInProgress}
+          aria-label='Register your account'
         >
-          {!registrationFormData.active && !registrationFormData.success && (
-            <span>Sign Up</span>
+          {registrationInProgress ? (
+            <LoadingSpinner />
+          ) : (
+            <span>Register Now</span>
           )}
-          {registrationFormData.active && (
-            <span className='flex items-center justify-center'>
-              <LoadingSpinner width={'w-5'} height={'h-5'} />
-            </span>
-          )}
-          {registrationFormData.success && <span>Success!</span>}
         </button>
       </div>
 
       {registerError && (
-        <div className='text-center'>
-          <span className='text-red-700 font-semibold'>REGISTER FAILED</span>
+        <div
+          role='alert'
+          aria-live='assertive'
+          className='text-center text-error-red'
+          id='form-error'
+        >
+          <span className='font-semibold' id='register-error'>
+            {registerError || 'REGISTRATION FAILED'}
+          </span>
         </div>
       )}
 
-      <p className='font-light text-gray-500 dark:text-gray-400'>
-        Already a member?{' '}
-        <Link
-          to='/sign-up'
-          className='font-medium text-colour2 hover:underline'
-        >
-          Login Now
-        </Link>
-      </p>
+      <div className='grid justify-center'>
+        <p className='font-light text-gray-500 dark:text-gray-400'>
+          Already a member?{' '}
+          <Link
+            to={LOGIN_PAGE_URL}
+            className={LinkStyle}
+            aria-label='Go to login page'
+          >
+            Login Now
+          </Link>
+        </p>
+      </div>
     </form>
   );
 }
